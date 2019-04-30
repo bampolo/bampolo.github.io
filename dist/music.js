bc5c76183e27f6dc2a4bdc005d676130bf576282
bampolo <bampolo@outlook.com>
2019-04-30 11:47:29 +0800

    Site updated: 2019-04-30 11:47:28

--- dist/music.js
+++ dist/music.js
@@ -4,18 +4,6 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'https://bampolo.github.io/images/avatar.png',
-      },
-      {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'https://bampolo.github.io/images/avatar.png',
-      },
-      {
         name: '风筝误',
         artist: '刘珂矣',
         url: 'http://up.mcyt.net/?down/46644.mp3',
